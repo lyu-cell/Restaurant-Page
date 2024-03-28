8e26865775a3978fac6431c5469e220bd69f3fe6
lyu-cell <shizueizawa340@gmail.com>
2024-03-28 19:49:19 +0600

    updated and added new styles to the header of the website

--- src/index.js
+++ src/index.js
@@ -10,21 +10,25 @@ const header = (function() {
     const homeBtn = document.createElement("button");
     const menuBtn = document.createElement("button");
     const contactBtn = document.createElement("button");
-    const logo = document.createElement("div")
+    const logo = document.createElement("div");
+    const reserveBtn = document.createElement("button");
 
     // classes are added here...
     homeBtn.classList.add("homeBtn");
     menuBtn.classList.add("menuBtn");
     contactBtn.classList.add("contactBtn");
-    logo.classList.add("logo")
+    logo.classList.add("logo");
+    reserveBtn.classList.add("reserve");
 
     homeBtn.textContent = "Home";
     menuBtn.textContent = "Menu";
     contactBtn.textContent = "Contact"
     logo.textContent = "Lyu&Lunch"
+    reserveBtn.textContent = "Reserve"
 
     header.appendChild(logo)
     nav.appendChild(homeBtn)
     nav.appendChild(menuBtn)
     nav.appendChild(contactBtn)
+    nav.appendChild(reserveBtn)
 })()
